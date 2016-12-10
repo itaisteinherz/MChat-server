71284e39ad89adaf27f3b0c45d02652be916e2a1
Itai Steinherz <itaisteinherz@gmail.com>
2016-12-11 00:10:56 +0200

    Update test/scripts/create-cert.sh
    Updated the test/scripts/create-cert.sh file to use the default values the certificates, instead of using a custom name.

--- test/scripts/create-cert.sh
+++ test/scripts/create-cert.sh
@@ -7,7 +7,7 @@ cd ssl
 
 # Create certificates
 
-openssl req -newkey rsa:2048 -new -nodes -sha256 -keyout key.pem -out csr.pem -subj /C=AU/ST=Some-State/O=Internet\ Widgits\ Pty\ Ltd/CN=Itais-MacBook-Air.local # TODO: Find a way to make this certificate valid in safari.
+openssl req -newkey rsa:2048 -new -nodes -sha256 -keyout key.pem -out csr.pem -subj /C=AU/ST=Some-State/O=Internet\ Widgits\ Pty\ Ltd
 openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out server.crt
 
 # Rename certificates
